96ad90a34504523026e3d9cfcceddddac83b57f6
RohitSinghGusain17 <rohitgusain672432@gmail.com>
2026-02-04 09:32:11 +0530

    code updated

--- AssessmentPractice/ExceptionHandling/RethrowingException/Program.cs
+++ AssessmentPractice/ExceptionHandling/RethrowingException/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+
+public class ExceptionRethrow
+{
+    public static void Main()
+    {
+        try
+        {
+            ProcessData();
+        }
+        catch (Exception ex)
+        {
+            // Log exception
+            Console.WriteLine("Caught in Main:"+ex.Message);
+        }
+    }
+
+    public static void ProcessData()
+    {
+        try
+        {
+            int.Parse("ABC");
+        }
+        catch (Exception ex)
+        {
+            // Log exception
+            Console.WriteLine("Logging in ProcessData: "+ex.Message);
+            // Rethrow while preserving stack trace
+            throw;
+        }
+    }
+}
